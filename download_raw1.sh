6c020671a65e036f631a3ef03ddfca7e06b9fa08
jwuw001 <103218589+jwuw001@users.noreply.github.com>
2022-12-05 09:28:47 -0800

    Update download_raw1.sh
    corrected code to what I used

--- download_raw1.sh
+++ download_raw1.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash -l
 
-#SBATCH -p batch --nodes=1 --ntasks=1 --cpus-per-task=12 --mem-per-cpu=24G --time=2-00:00:00 --job-name="MetagenomeEpiHealth1" --out logs/MAG.EH.1.%a.log
+#SBATCH -p batch --nodes=1 --ntasks=1 --cpus-per-task=12 --mem-per-cpu=12G --time=4:00:00 --job-name="MetagenomeTest1" --out logs/MAG.test.1.%a.log
 
 module load sratoolkit/3.0.0 
 module load parallel-fastq-dump/0.6.7 
 
-parallel-fastq-dump --sra-id SRR12131164 --threads 4 --outdir out/ --split-files --gzip
+parallel-fastq-dump --sra-id SRR12131175 --threads 4 --outdir out/ --split-files --gzip
+
 
 #this is what I tried running first
