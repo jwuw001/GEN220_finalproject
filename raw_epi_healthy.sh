1f63c6408593c72be600e8e0f2f97a611acc74ef
jwuw001 <103218589+jwuw001@users.noreply.github.com>
2022-12-05 09:35:28 -0800

    Update raw_epi_healthy.sh
    increased memory for job from 24-36 gb

--- raw_epi_healthy.sh
+++ raw_epi_healthy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash -l
 
-#SBATCH -p batch --nodes=1 --ntasks=1 --cpus-per-task=12 --mem-per-cpu=24G --time=2-00:00:00 --job-name="MetagenomeEpiHealth1" --out logs/MAG.EH.1.%a.log
+#SBATCH -p batch --nodes=1 --ntasks=1 --cpus-per-task=12 --mem-per-cpu=36G --time=2-00:00:00 --job-name="MetagenomeEpiHealth1" --out logs/MAG.EH.1.%a.log
 
 module load sratoolkit/3.0.0 
 module load parallel-fastq-dump/0.6.7 
